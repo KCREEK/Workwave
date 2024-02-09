72ca129553f96cdcae0be054264c6f8c8493feac
KCREEK <siddhantrakibe@gmail.com>
2024-02-10 03:22:33 +0530

    added service and bootstrap dependencies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    {path: '', component: DashboardComponent}
+    {path: '', component: DashboardComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
