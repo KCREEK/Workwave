f278db549250d8d3345d8acdbed7bbc7f1fc4f9c
KCREEK <siddhantrakibe@gmail.com>
2024-02-11 04:04:24 +0530

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { JobsComponent } from './jobs/jobs.component';
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent}
+    {path:'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'jobs', component: JobsComponent}
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { NgIf } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, NgIf, FormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
+  errorMessage: string | null = null;
 
-}
+  constructor() { } 
+
+  onRememberMeChange(newValue: boolean) {
+    this.rememberMe = newValue;
+  }  
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    const loginData = {
+      username: this.username,
+      password: this.password
+  };
+}}
